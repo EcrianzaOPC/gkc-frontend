547ecdf7f884c453f9ec628c119bde6fab758fb9
abhik-99 <abhik.banerjee.1999@gmail.com>
2021-10-09 22:33:26 +0530

    Contract invocation change and UI Channges.

--- src/App.js
+++ src/App.js
@@ -17,9 +17,10 @@ import {
   Box,
   Avatar,
   Grid,
-  IconButton
+  // IconButton,
+  TextField
 } from '@material-ui/core';
-import { Add, Remove } from "@material-ui/icons";
+// import { Add, Remove } from "@material-ui/icons";
 
 import Web3 from "web3";
 import Web3Modal from "web3modal";
@@ -27,7 +28,8 @@ import Web3Modal from "web3modal";
 import Authereum from "authereum";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
-import backgroundImg from "./assets/img/minthome2.jpg";
+// import backgroundImg from "./assets/img/minthome2.jpg";
+import backgroundImg from "./assets/img/minthome_final.jpg";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -43,14 +45,15 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center'
   },
   paper: {
-    width: '300px',
-    height: '400px',
+    width: '400px',
+    height: '500px',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-evenly',
     flexDirection: 'column'
   },
   box: {
+    display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center'
   },
@@ -168,13 +171,13 @@ function App() {
     await getweb3().then(async (web3) => {
 
 
-      const address = "0x34946aD91Bd03D1d6289C5C14C1fA21dDce46CD9";
+      const address = "0x9AaE182E9AA6594D05a30395ff49455e5fAD929B";
 
       const abi = [{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"},{"internalType":"uint256","name":"_maxNftSupply","type":"uint256"},{"internalType":"uint256","name":"saleStart","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"GKC_PROVENANCE","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MAX_NFT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"REVEAL_TIMESTAMP","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"emergencySetStartingIndex","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"flipSaleState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isSaleActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxNftPurchase","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nftPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"reserve","type":"uint256"}],"name":"reserveNft","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"provenance","type":"string"}],"name":"setProvenance","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"newTimestamp","type":"uint256"}],"name":"setReveealTimestamp","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"setStartingIndex","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"startingIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"startingIndexBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
       const contract = new web3.eth.Contract(abi, address);
       console.log("Contract", contract);
-      await contract.methods.mint(quantity).send({from: accInfo, value: 0.02 * quantity * Math.pow(10,18)});
+      await contract.methods.mint(quantity).send({from: accInfo, value: 0.02 * parseInt(quantity) * Math.pow(10,18)});
     })
   }
 
@@ -212,25 +215,34 @@ function App() {
               </>
             }
             <Box className={classes.box}>
-            <Button variant='contained' onClick={connectWallet} disabled={loggedIn}>
-              Connect
-            </Button>
-            <Button variant='outlined' onClick={disconnectWallet} disabled={!loggedIn}>
-              Disconnect
-            </Button>
+              <Button variant='contained' onClick={connectWallet} disabled={loggedIn}>
+                Connect
+              </Button>
+              <Button variant='outlined' onClick={disconnectWallet} disabled={!loggedIn}>
+                Disconnect
+              </Button>
             </Box>
 
-            <Box>
-              <IconButton variant="contained" color="primary" disabled={!loggedIn} onClick={() => quantity < 50 ? setQuantity(quantity + 1) : 50}>
+            <Box className={classes.box}>
+              {/* <IconButton variant="contained" color="primary" disabled={!loggedIn} onClick={() => quantity < 50 ? setQuantity(quantity + 1) : 50}>
                 <Add />
-              </IconButton>
-              <Typography> {quantity} </Typography>
-              <IconButton variant="contained" color="primary" disabled={!loggedIn} onClick={() => quantity > 1 ? setQuantity(quantity - 1) : 1}>
-                <Remove />
-              </IconButton>
+              </IconButton> */}
+              <Button variant="outlined" color="primary" onClick={() => setQuantity(5)}>
+                5
+              </Button>
+              <Button variant="outlined" color="primary" onClick={() => setQuantity(10)}>
+                10
+              </Button>
+              <Button variant="outlined" color="primary" onClick={() => setQuantity(15)}>
+                15
+              </Button>
+              <Button variant="outlined" color="primary" onClick={() => setQuantity(20)}>
+                20
+              </Button>
             </Box>
-
-
+            <Box className={classes.box}>
+              <TextField fullWidth value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+            </Box>
             <Button variant='contained' color='secondary' onClick={mint} disabled={!loggedIn}>
               Mint
             </Button>
